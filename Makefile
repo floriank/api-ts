5e5739d29f9171bfb13b31b83cd19d2cf26613af
Florian Kraft <floriankraft@gmx.de>
2016-03-15 12:39:27 +0100

    define initial routes for the api

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
-all:
+all: deps build
+build:
+	./node_modules/.bin/tsc --sourcemap --module commonjs ./bin/www.ts
+deps:
 	npm i
 	typings install
-	./node_modules/.bin/tsc --sourcemap --module commonjs ./bin/www.ts
 clean:
 	rm -rf bin/www.js
 	rm -rf node_modules
--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
 /// <reference path='./typings/main.d.ts' />
 
 import * as express from "express";
+import routes = require("./routes");
 
 var app = express();
+
+app.use('/', routes);
+
+
+// catch the remaining routes as errors
+app.use((req, res, next) => {
+  var error = new Error('Not found');
+  error['status'] = 404;
+  next(error);
+});
+
 
 export = app;
